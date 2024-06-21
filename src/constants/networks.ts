4c0cc5668bbd600bbf744b848fd89a92cdaab3bd
swag <105200320+sw4geth@users.noreply.github.com>
2024-06-21 12:45:52 +0700

    Update networks.ts

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,5 +1,5 @@
-type NetworkNames = 'MAINNET' | 'RINKEBY' | 'GOERLI' | 'POLYGON' | 'MUMBAI' | 'ROPSTEN';
-type NetworkIDs = '1' | '3' | '4' | '5' | '137' | '80001';
+type NetworkNames = 'MAINNET' | 'RINKEBY' | 'GOERLI' | 'POLYGON' | 'MUMBAI' | 'ROPSTEN' | 'BASE' | 'ZORA' ;
+type NetworkIDs = '1' | '3' | '4' | '5' | '137' | '80001' | '8453' | '7777777';
 
 // Supported networks with Zora contract deployments.
 // As more networks are supported by zora more network IDs will be added.
@@ -10,6 +10,8 @@ const Networks: Record<NetworkNames, NetworkIDs> = {
   GOERLI: '5',
   POLYGON: '137',
   MUMBAI: '80001',
+  BASE: '8453',
+  ZORA: '7777777'
 };
 
 export { Networks };
